2f571dbd5a9d26b5198f56056c88002d04f194cb
Vitali Yablonski <vitali.yablonski.consultant@consumer.org>
2016-02-25 13:34:54 +0300

    init jmp/module-12

---
+++ module-12/src/main/java/jmp/module12/Main.java
@@ -0,0 +1,16 @@
+package jmp.module12;
+
+/**
+ * The main class
+ */
+public class Main {
+
+    /**
+     * The main method
+     * @param vmArguments a array of programm arguments
+     */
+    public static void main(String[] vmArguments) {
+        System.out.println("m12");
+    }
+
+}
