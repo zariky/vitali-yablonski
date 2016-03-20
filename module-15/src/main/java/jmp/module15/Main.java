39a45b10db0ce48a1b80fe9441e6f75927cf4316
vitali_yablonski <ya.vitalic@gmail.com>
2016-03-21 01:06:13 +0300

    init jmp/module-15

--- module-15/src/main/java/jmp/module15/Main.java
+++ module-15/src/main/java/jmp/module15/Main.java
@@ -1,12 +1,6 @@
 package jmp.module15;
 
-import jmp.module15.clients.fifa.ArrayOftPlayersWithCards;
-import jmp.module15.clients.fifa.Info;
-import jmp.module15.clients.fifa.InfoSoapType;
-import jmp.module15.clients.fifa.TPlayersWithCards;
-import jmp.module15.clients.fifa.TTeamInfo;
-import jmp.module15.clients.weather.GlobalWeather;
-import jmp.module15.clients.weather.GlobalWeatherSoap;
+import jmp.module15.services.SportGameService;
 
 /**
  * The main class
@@ -18,41 +12,22 @@ public class Main {
      * @param vmArguments a array of programm arguments
      */
     public static void main(String[] vmArguments) {
-        Info footballService = new Info();
-        InfoSoapType fifaSoap = footballService.getInfoSoap();
+        SportGameService sportGameService = new SportGameService();
 
-        GlobalWeather weatherService = new GlobalWeather();
-        GlobalWeatherSoap weatherSoap = weatherService.getGlobalWeatherSoap();
-
-        findWeathers(weatherSoap, fifaSoap);
-        findTeams(fifaSoap);
-        findInfoPlayers(fifaSoap, "Uruguay");
-    }
-
-    private static void findWeathers(GlobalWeatherSoap weatherSoap, InfoSoapType fifaSoap) {
         System.out.println("Weathers:");
+        println(sportGameService.weathers());
 
-        for (String city : fifaSoap.cities().getString()) {
-            System.out.println(weatherSoap.getWeather(city, ""));
-        }
-    }
-
-    private static void findTeams(InfoSoapType fifaSoap) {
         System.out.println("FIFA teams:");
+        println(sportGameService.teams());
 
-        for (TTeamInfo teamInfo : fifaSoap.teams().getTTeamInfo()) {
-            System.out.println(teamInfo.getSName());
-        }
+        final String TEAM = "Uruguay";
+        System.out.println("Players(" + TEAM + "):");
+        println(sportGameService.players(TEAM));
     }
 
-    private static void findInfoPlayers(InfoSoapType fifaSoap, String team) {
-        System.out.println("Players(" + team + "):");
-
-        ArrayOftPlayersWithCards allPlayers = fifaSoap.allPlayersWithYellowOrRedCards(false, false, false);
-        for (TPlayersWithCards player : allPlayers.getTPlayersWithCards()) {
-            if (player.getSTeamName().equals(team)) {
-                System.out.println("Player = " + player.getSName()  + ", YellowCards = " + player.getIYellowCards() + ", RedCards = " + player.getIRedCards());
-            }
+    private static void println(String[] array) {
+        for (String entity : array) {
+            System.out.println(entity);
         }
     }
 
